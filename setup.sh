73b2e300b64dac821a5bb618ac216bd3926461ea
sumancvb <sumancvb@gmail.com>
2019-06-14 23:47:33 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,7 +19,8 @@ unclutter -idle 0.5 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk https://fast.com https://google.com &
+#/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk https://fast.com &
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk https://192.168.1.17 &
 
 while true; do
    xdotool keydown ctrl+r; xdotool keyup ctrl+r;
